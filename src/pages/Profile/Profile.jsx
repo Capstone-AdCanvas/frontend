18fd1a4e7ffb13cca3c1d0403e786cfa82050613
t.jin_01 <601135@naver.com>
2025-03-28 21:23:31 +0900

    design: 프로필 페이지 퍼블리싱-1

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,11 +1,43 @@
-import React from "react";
+import "./Profile.css";
+import editIcon from "../../assets/profile-edit-btn-1.png";
+import profileIcon from "../../assets/profile-icon.png";
+import instaIcon from "../../assets/profile-insta-icon.png";
 
 function Profile() {
   return (
-    <div>
-      <h1>Profile Page</h1>
-      <p>View and edit your profile.</p>
-    </div>
+    <section className="profilePage">
+      <h2 className="profilePage__title">My Profile</h2>
+      <article className="profilePage__content">
+        <div className="profilePage__content__btn">
+          <img src={editIcon} alt="프로필 수정 버튼" />
+        </div>
+        <div className="profilePage__content__image">
+          <img src={profileIcon} alt="" />
+        </div>
+        <div className="profilePage__content__info">
+          <div className="profilePage__content__info__name">
+            닉네임:
+            <span className="profilePage__content__info__name__nickname">
+              Chill guy
+            </span>
+          </div>
+          <div className="profilePage__content__info__btn">
+            <button className="profilePage__content__info__btn__image">
+              Image 3
+            </button>
+            <button className="profilePage__content__info__btn__video">
+              Video 2
+            </button>
+          </div>
+          <div className="profilePage__content__info__insta">
+            <img src={instaIcon} alt="" />
+            <span className="profilePage__content__info__insta__name">
+              chill_guy
+            </span>
+          </div>
+        </div>
+      </article>
+    </section>
   );
 }
 
