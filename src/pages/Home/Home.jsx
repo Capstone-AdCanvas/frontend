a5e3647eb8e79a0a388ed8f41e2ebcc7d550d2af
장원진 <jjwj0228@naver.com>
2025-03-31 21:26:43 +0900

    design: AI Image, AI Video 버튼 디자인 완료

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,28 @@
 import React from "react";
+import "./Home.css";
 
 function Home() {
   return (
-    <div>
-      <h1>Home Page</h1>
-      <p>Welcome to the Home page.</p>
+    <div className="homePage">
+      <div className="homePage__title">
+        <p className="home-subtitle">Redifinig Ad Creation with AI</p>
+        <h3 className="home-title">Personal AI Creative Studio</h3>
+      </div>
+      <div className="AI_buttons">
+        <button className="AI_buttons__image">
+          <div className="AI_buttons__image--inner">
+            <h3>AI Image</h3>
+            <p>Turn Ideas into image</p>
+        </div>
+        </button>
+        <button className="AI_buttons__video">
+        <h3>AI Video</h3>
+        <p>Turn Ideas into Video</p>
+          </button>
+      </div>
+      <div className="others">
+        
+      </div>
     </div>
   );
 }
