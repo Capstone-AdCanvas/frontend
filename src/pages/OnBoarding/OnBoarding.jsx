8b38d682efc4928df778b5e055b88fc135eb7288
t.jin_01 <601135@naver.com>
2025-04-10 18:07:50 +0900

    design: 온보딩페이지2, 3 퍼블리싱

--- src/pages/OnBoarding/OnBoarding.jsx
+++ src/pages/OnBoarding/OnBoarding.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import "./OnBoarding.css";
 import OnBoardingPage1 from "./Page1/OnBoardingPage1";
+import OnBoardingPage2 from "./Page2/OnBoardingPage2";
+import OnBoardingPage3 from "./Page3/OnBoardingPage3";
 
 const OnBoarding = () => {
   return (
     <section className="onboardingPage">
       <OnBoardingPage1 />
+      <OnBoardingPage2 />
+      <OnBoardingPage3 />
     </section>
   );
 };
