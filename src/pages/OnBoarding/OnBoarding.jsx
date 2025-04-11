fcdc16b8469102b44f3acdb3f68468feec997072
t.jin_01 <601135@naver.com>
2025-04-11 18:10:28 +0900

    design: 온보딩 페이지 4,5 구현중

--- src/pages/OnBoarding/OnBoarding.jsx
+++ src/pages/OnBoarding/OnBoarding.jsx
@@ -3,6 +3,8 @@ import "./OnBoarding.css";
 import OnBoardingPage1 from "./Page1/OnBoardingPage1";
 import OnBoardingPage2 from "./Page2/OnBoardingPage2";
 import OnBoardingPage3 from "./Page3/OnBoardingPage3";
+import OnBoardingPage4 from "./Page4/OnBoardingPage4";
+import OnBoardingPage5 from "./Page5/OnBoardingPage5";
 
 const OnBoarding = () => {
   return (
@@ -10,6 +12,8 @@ const OnBoarding = () => {
       <OnBoardingPage1 />
       <OnBoardingPage2 />
       <OnBoardingPage3 />
+      <OnBoardingPage4 />
+      <OnBoardingPage5 />
     </section>
   );
 };
---
+++ src/pages/OnBoarding/Page5/OnBoardingPage5.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "./OnBoardingPage5.css";
+
+const OnBoardingPage5 = () => {
+  return (
+    <article className="onboardingPage5">
+      <div className="onboardingPage5__content">
+        <div className="onboardingPage5__content__introduce">
+          <span className="title1">효율적인</span>
+          <span className="title2">디지털 마케팅 서비스를</span>
+          <span className="title3">제공합니다</span>
+          <span className="title4">
+            <span>AdCanvas</span> 와 함께 시작하세요!
+          </span>
+        </div>
+        <button className="onboardingPage5__button">
+          AdCanvas와
+          <br />
+          함께 시작하기
+        </button>
+      </div>
+    </article>
+  );
+};
+
+export default OnBoardingPage5;
